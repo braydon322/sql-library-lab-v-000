e7ffb50132d03cf7cea6cd45ce5b28f327072675
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-25 01:22:29 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,7 +3,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, motto, length(motto) AS length_of_motto FROM characters ORDER BY length_of_motto DESC ;"
+  "SELECT name, motto, length(motto) AS length_of_motto FROM characters ORDER BY length_of_motto DESC;"
 end
 
 
