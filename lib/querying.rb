3e50deee5b379b2a4b4ffcbc0411faa52f889509
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-25 01:31:43 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,7 +11,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT title, name FROM series INNER JOIN subgenres ON series.subgenre_id = subgenres.id;"
+  "SELECT title, name FROM authors INNER JOIN subgenres"
 end
 
 def select_series_title_with_most_human_characters
