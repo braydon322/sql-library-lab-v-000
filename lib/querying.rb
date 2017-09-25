78491fbd86790c35931de2941e5bc9ac065be083
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-25 02:21:24 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,6 +20,7 @@ def select_name_and_series_subgenres_of_authors
    INNER JOIN authors
    ON authors.id = series.author_id
    INNER JOIN subgenres
+   ON series.subgenre_id = subgenres.id
    GROUP BY authors.name;"
   #
   # # #SUBGENRES
