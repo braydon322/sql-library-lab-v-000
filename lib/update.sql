c5a92a8947f7ef688b9fe7f8416f2c8dddd8701d
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-25 02:28:06 +0000

    automatically backed up by learn

--- lib/update.sql
+++ lib/update.sql
@@ -1,5 +1,1 @@
-UPDATE characters
-   SET species = 'Martian'
- WHERE (SELECT * FROM characters ORDER BY characters.name DESC LIMIT 1);
-
 UPDATE chracters set species = 'Martian' WHERE id = (SELECT MAX(id) FROM characters)
